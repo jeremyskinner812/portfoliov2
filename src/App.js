9e281b0d93c0ecd0a7dca2f6193bcc62c1b030a8
Jeremy <jdskinne@gmail.com>
2023-01-30 22:26:55 -0600

    set up basic react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header>
+        <Nav></Nav>
+      </Header>
+      <main></main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
